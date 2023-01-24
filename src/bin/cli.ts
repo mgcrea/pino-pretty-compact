8fdd99514cbd6c548d57adc1d9cd78da3bad8f77
Olivier Louvignes <olivier@mgcrea.io>
2023-01-24 15:33:27 +0100

    feat(bin): add pino-pretty-compact cli

---
+++ src/bin/cli.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env ts-node
+
+import { pipeline } from "node:stream/promises";
+import { build } from "../prettifier";
+
+const stream = build({});
+
+const main = async () => {
+  await pipeline(process.stdin, stream);
+};
+
+main();
