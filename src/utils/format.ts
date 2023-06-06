fd1a7cbfb67db1caae1e0decc3b3f07aef5b5881
Olivier Louvignes <olivier@mgcrea.io>
2023-06-06 09:53:19 +0200

    chore(formatError): inherit message parent level when logging errors

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -54,7 +54,7 @@ export const formatPlugin = (plugin: string): string => color.gray(`(${plugin})`
 export const formatErrorStack = (stack: string): string =>
   color.gray(stack.replace(CWD_REGEX, ".").split(EOL).slice(1).join(EOL));
 
-export const formatError = (error: SerializedError): string => {
+export const formatError = (error: SerializedError, level: LOG_LEVEL): string => {
   const { statusCode = 500, type = error["name"], stack = `${EOL}    at ???` } = error;
   const supportsArt = color.options.supportLevel === 2; /* SupportLevel.ansi256 */
   const icon = supportsArt ? "×" : "x";
@@ -63,7 +63,7 @@ export const formatError = (error: SerializedError): string => {
   const output = [color[isInternalError ? "red" : "yellow"](`${icon}${type} `), color.magenta(statusCode)];
 
   if (isInternalError) {
-    output.push(color.red(`: ${error.message}`), EOL, formatErrorStack(stack));
+    output.push(colorForLevel(level)(`: ${error.message}`), EOL, formatErrorStack(stack));
   } else {
     output.push(`: ${error.message}`);
   }
